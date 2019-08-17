dd1e735edec405e63f0ded9f8730e22ebd3d31e4
charbo23 <charbo233@qq.com>
2019-08-17 15:51:48 +0800

    TodoList完成

--- demo01/src/TodoList.js
+++ demo01/src/TodoList.js
@@ -1,33 +1,141 @@
 import React, { Component } from 'react';
-import { Input, Button, List } from 'antd';
+import axios from "axios";
+import { Input, Button, List, Icon } from 'antd';
 import './style.css';
 import store from "./store";
+
+// class IconText extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.handleClick = this.handleClick.bind(this);
+//     }
+//     render() {
+//         return (
+//             <span onClick={this.handleClick}>
+//                 <Icon type={this.props.type} style={{ marginRight: 8 }} />
+//                 {this.props.text}
+//             </span>
+//         );
+//     }
+//     handleClick() {
+//         this.props.deleteItem();
+
+//     }
+// }
+const IconText = ({ type, text, handleClick, itemId }) => (
+    <span onClick={handleClick} item-id={itemId}>
+        <Icon type={type} style={{ marginRight: 8 }} />
+        {text}
+    </span>
+);
 
 class TodoList extends Component {
     constructor(props) {
         super(props);
+        this.changeInputValue = this.changeInputValue.bind(this);
         this.state = store.getState();
+        this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+        this.storeChange = this.storeChange.bind(this);
+        this.onKeyUp = this.onKeyUp.bind(this);
+        store.subscribe(this.storeChange);
+    }
+    componentDidMount() {
+        axios.get('https://www.easy-mock.com/mock/5d57aacb8814035379b71cd0/ReduxDemo01/tasks')
+            .then((res) => {
+                // console.log(`axios：数据获取成功，内容\n${JSON.stringify(res.data.data.list)}`);
+                // this.setState({
+                //     list: res.data.data.list,
+                //     loading:false
+                // })
+
+                const action = {
+                    type: 'init',
+                    list: res.data.data.list
+                }
+                store.dispatch(action);
+            })
+            .catch((error) => {
+                console.log(`axios：数据获取失败，内容 ${error}`);
+            })
     }
     render() {
         return (
             <div>
                 <div className='input-group'>
                     <Input
-                        placeholder={this.state.inputValue} style={{ width: '250px' }}
+                        placeholder={this.state.inputValue}
+                        style={{ width: '250px' }}
+                        onChange={this.changeInputValue}
+                        onKeyUp={this.onKeyUp}
+                        value={this.state.inputValue}
                     />
-                    <Button type='primary'>增加</Button>
+                    <Button
+                        type='primary'
+                        onClick={this.addItem}
+                    >增加</Button>
                 </div>
                 <div className='list'>
                     <List
                         bordered
+                        loading={this.state.loading}
                         dataSource={this.state.list}
                         renderItem={(item) => (
-                            <List.Item>{item}</List.Item>
+                            <List.Item
+                                actions={[
+                                    <IconText
+                                        type="delete"
+                                        text="删除"
+                                        key="list-delete"
+                                        handleClick={this.deleteItem}
+                                        itemId={item.id}
+                                    />
+                                    // <a onClick={this.deleteItem} key="list-loadmore-edit">删除</a>
+                                ]}
+                            >
+                                {item.value}
+                            </List.Item>
                         )}
                     />
                 </div>
             </div>
         );
+    }
+
+    changeInputValue(e) {
+        const action = {
+            type: 'changeInput', //动作名称，一般为常量
+            value: e.target.value
+        }
+        store.dispatch(action);
+    }
+    onKeyUp(e) {
+        if (e.keyCode === 13) {
+            this.addItem()
+        }
+    }
+    storeChange() {
+        this.setState(store.getState);
+    }
+
+    addItem() {
+        if (!this.state.inputValue) {
+            return;
+        }
+
+        const action = {
+            type: 'addItem'
+        }
+        store.dispatch(action);
+    }
+
+    deleteItem(e) {
+        let itemId = e.target.getAttribute('item-id');
+        const action = {
+            type: 'deleteItem',
+            itemId: itemId
+        }
+        store.dispatch(action);
     }
 }
 
--- demo01/src/store/reducer.js
+++ demo01/src/store/reducer.js
@@ -1,11 +1,45 @@
+import _ from 'lodash';
+import shortid from 'shortid';
 const defaultState = {
-    inputValue: 'Write Something',
+    inputValue: '',
     list: [
-        '早8点开晨会，分配今天的代码任务',
-        '早9点和项目经理开需求沟通会',
-        '晚5点半对今日代码进行review'
-    ]
+        // '早8点开晨会，分配今天的代码任务',
+        // '早9点和项目经理开需求沟通会',
+        // '晚5点半对今日代码进行review'
+    ],
+    loading: true
 };
 export default (state = defaultState, action) => {
+
+    //Reducer里只能接收state不能改变state
+
+    if (action.type === 'init') {
+        let newState = _.cloneDeep(state);
+        newState.list = action.list;
+        newState.loading = false;
+        return newState;
+    }
+    if (action.type === 'changeInput') {
+        let newState = _.cloneDeep(state);
+        newState.inputValue = action.value;
+        return newState;
+    }
+
+    if (action.type === 'addItem') {
+        let newState = _.cloneDeep(state);
+        newState.list.push(
+            {
+                id: shortid.generate(),
+                value: newState.inputValue
+            }
+        );
+        newState.inputValue = '';
+        return newState;
+    }
+    if (action.type === 'deleteItem') {
+        let newState = _.cloneDeep(state);
+        newState.list = newState.list.filter((listItem) => { return listItem.id !== action.itemId });
+        return newState;
+    }
     return state;
 }
