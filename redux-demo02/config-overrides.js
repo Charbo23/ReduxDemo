046a45a88efa0d4df9b0e3aa96140f83a04d72fa
charbo23 <charbo233@qq.com>
2019-08-20 22:25:52 +0800

    使用less-loader并自定义antd主题色

--- redux-demo02/config-overrides.js
+++ redux-demo02/config-overrides.js
@@ -1,8 +1,12 @@
 const { override, fixBabelImports } = require('customize-cra');
- module.exports = override(
-   fixBabelImports('import', {
-     libraryName: 'antd',
-     libraryDirectory: 'es',
-     style: 'css', // `style: true` 会加载 less 文件
-   }),
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: 'css', // style: true 会加载 less 文件，,不加引号,且必须添加addLessLoader！！
+  }),
+  // addLessLoader({
+  //   javascriptEnabled: true,
+  //   modifyVars: { '@primary-color': '#1DA57A' },
+  // }),
 );
