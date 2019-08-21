63575b6f5ca57cc54e7f3ef008ad300c4fa1c446
charbo23 <charbo233@qq.com>
2019-08-21 17:53:25 +0800

    尝试react app

--- redux-demo02/src/index.js
+++ redux-demo02/src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import TodoList from './TodoList';
 import { Provider } from 'react-redux';
 import store from './store';
+import * as serviceWorker from './serviceWorker';
 
 const App = (
     <Provider store={store}>
         <TodoList />
     </Provider>
 )
+
 ReactDOM.render(App, document.getElementById('root'));
+
+//可让网站像一个应用一样离线访问，但只有在localhost或https协议下才有效
+serviceWorker.register();
